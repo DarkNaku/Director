2204b9883d43a84bb37374e264434e5676529de5
DarkNaku <skyrack@naver.com>
2024-11-11 13:16:18 +0900

    빌드 오류 수정

--- Assets/Director/Runtime/Director.cs
+++ Assets/Director/Runtime/Director.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
